15353a96b9c02cce1c469e2b5a515dd22eaca382
Anna <annasofia0017@gmail.com>
2026-04-07 00:46:53 -0500

    Add the follow system with followers/follwing count,user lists, and databse migration

--- newApp/models.py
+++ newApp/models.py
@@ -15,4 +15,12 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
-    
+#this is the follow model which will be used to create the follow and unfollow functionality
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.following.username}"
--- newApp/urls.py
+++ newApp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('profile/', views.profile, name = 'profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name = 'login'),
     path('logout/', views.logout_view, name='logout'),#redirect to home after logout
-    path('main_home/', views.main_home, name='main_home') #created this 
-   
+    path('main_home/', views.main_home, name='main_home'), #created this  
+    path("follow/<str:username>/", views.follow_view, name="follow_view"),#this is the follow view with the username
+    path("user/<str:username>/", views.user_profile, name="user_profile"),#this is the user profile view with the username
 ]
