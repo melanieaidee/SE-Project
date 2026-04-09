b85ec7fd7e912c4f1e0c626a61fbaaacae112437
melanie-flores <melanie.flores03@utrgv.edu>
2026-04-09 12:39:48 -0500

    Update templates, URLs, settings, and static files

--- newApp/urls.py
+++ newApp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+from newApp import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('register/', views.register, name = 'register'),
     path('profile/', views.profile, name = 'profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name = 'login'),
+    path('signup/', views.signup, name = 'signup'),
     path('logout/', views.logout_view, name='logout'),#redirect to home after logout
     path('main_home/', views.main_home, name='main_home') #created this 
    
--- springApp/settings.py
+++ springApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'newApp.apps.NewappConfig',
+    'springApp',
 ]
 #will go to NewApp -> then apps.py -> NewappConfig
 MIDDLEWARE = [
